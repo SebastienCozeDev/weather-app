b54c70096dcb56520f4ea8440a47d8c225d83475
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2024-04-18 18:38:39 +0200

    Create the layout

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import {Home} from "./pages/Home";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import {style} from "./App.style";
+import { ImageBackground } from "react-native";
+import backgroundImg from "./assets/background.png";
+import {StatusBar} from "expo-status-bar/build/StatusBar";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Home />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ImageBackground source={backgroundImg} style={style.backgroundImgContainer} imageStyle={style.backgroundImg}>
+      <SafeAreaProvider>
+        <SafeAreaView style={style.container}>
+          <Home />
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <StatusBar style="light" />
+    </ImageBackground>
   );
 }
---
+++ App.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+  backgroundImgContainer: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  backgroundImg: {
+    opacity: 0.7,
+
+  },
+  container: {
+    flex: 1,
+  },
+});
