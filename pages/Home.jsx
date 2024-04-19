c38a747b552fd641a266399d4c3757d040bfd03c
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2024-04-19 17:43:29 +0200

    Fetch weather data

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,6 +2,7 @@ import {Text, View} from "react-native";
 import {style} from "./Home.style";
 import {getCurrentPositionAsync, requestForegroundPermissionsAsync} from "expo-location";
 import {useState, useEffect} from "react";
+import {WeatherAPI} from "../api/weather";
 
 /**
  * Home component.
@@ -12,10 +13,17 @@ import {useState, useEffect} from "react";
  */
 export function Home() {
   const [coords, setCoords] = useState({});
+  const [weather, setWeather] = useState();
 
   useEffect(() => {
     getUserCoords().then();
   }, []);
+
+  useEffect(() => {
+    if (coords) {
+      fetchWeather(coords).then();
+    }
+  }, [coords]);
 
   /**
    * Get the user's coordinates.
@@ -31,6 +39,13 @@ export function Home() {
       setCoords({ lat: "48.85", lng: "2.35" });
     }
   }
+
+  async function fetchWeather(coordinates) {
+    const weatherResponse = await WeatherAPI.fetchWeatherFromCoords(coordinates);
+    setWeather(weatherResponse);
+  }
+
+  console.log(weather);
 
   return (
     <>
