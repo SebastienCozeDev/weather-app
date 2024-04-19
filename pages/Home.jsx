392551899e373730ed537db88356cf151770789b
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2024-04-19 17:23:19 +0200

    Add the GPS coords access

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,11 +1,30 @@
 import {Text, View} from "react-native";
 import {style} from "./Home.style";
+import {getCurrentPositionAsync, requestForegroundPermissionsAsync} from "expo-location";
+import {useState, useEffect} from "react";
 
 export function Home() {
+  const [coords, setCoords] = useState({});
+
+  useEffect(() => {
+    getUserCoords().then();
+  }, []);
+
+  async function getUserCoords() {
+    let {status} = await requestForegroundPermissionsAsync()
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoords({ lat: location.coords.latitude, lng: location.coords.longitude });
+    } else {
+      setCoords({ lat: "48.85", lng: "2.35" });
+    }
+  }
+
+  console.log(coords);
+
   return (
     <>
       <View style={style.basicWeatherContainer}>
-        <Text style={{ fontSize: 60, color: "white", }}>Hello</Text>
       </View>
       <View style={style.searchbarContainer}></View>
       <View style={style.advancedWeatherContainer}></View>
